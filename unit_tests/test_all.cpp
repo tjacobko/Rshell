5244f4ed96cb8e0bbf1e7cdbc9b0586ce0ed0bcd
Ethan Nguyen <engeo2558@gmail.com>
2019-11-13 19:18:37 -0800

    Updated test_all_cpp to include Git and Exit

--- unit_tests/test_all.cpp
+++ unit_tests/test_all.cpp
@@ -1,6 +1,9 @@
 #include "gtest/gtest.h"
 #include "ListDirectory_test.cpp"
-#include "MakeDirectory_test.cpp"
+#include "Git_test.cpp"
+#include "Echo_test.cpp"
+//#include "MakeDirectory_test.cpp"
+#include "Exit_test.cpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
