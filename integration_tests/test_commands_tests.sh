d0cfed634635ec07bf6a74224ca617b985223674
Ethan Nguyen <engeo2558@gmail.com>
2019-11-27 16:37:26 -0800

    fixed test integration test

--- integration_tests/test_commands_tests.sh
+++ integration_tests/test_commands_tests.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 ### test_commands_test ###
 
-
-
-test -e src/main.cpp && echo main.cpp FOUND
-test -f src/main.cpp && echo main.cpp FOUND
-test -d src/ && echo src directory FOUND
-[ -e src/main.cpp ] && echo main.cpp FOUND
-[ -f src/main.cpp ] && echo main.cpp FOUND
-[ -d src/ ] && echo src directory FOUND
+test -e exit_command_tests.sh && echo EXIT TEST FOUND
+test -f commented_command_tests.sh && echo COMMENT TEST FOUND
+test -d DIRECTORYDOESNOTEXIST || echo this directory should not be found
+[ -e exit_command_tests.sh ] && echo EXIT TEST FOUND
+[ -f commented_command_tests.sh ] && echo COMMENT TEST FOUND
+[ -d src/ ] || echo src DIRECTORY NOT FOUND
 mkdir TOREMOVE
 [ -d TOREMOVE/ ] && echo TOREMOVE directory FOUND
 rm -rf TOREMOVE
