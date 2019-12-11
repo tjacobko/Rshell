81b06bce7b488aead0bc9613ebbe79f16f55fbc7
Ethan Nguyen <engeo2558@gmail.com>
2019-12-10 19:00:12 -0800

    Changed execute to have inputs

--- src/Append.cpp
+++ src/Append.cpp
@@ -2,13 +2,14 @@
 #define __APPEND_CPP__
 
 #include "Append.h"
+#include "Base.h"
 
 #include "fcntl.h" //open()
 #include "sys/stat.h" //for open()
 
 Append::Append(Base* lhs, Base* rhs) : Base(lhs,rhs) {}
 
-bool Append::execute() {
+bool Append::execute(int in, int out) {
 	string outputFile = //read file from the rhs
 	open(outputFile.c_str(), O_WRONLY | O_APPEND | O_CREAT, S_IRSUR | S_IRGRP | S_IWUSR);
 		/*
@@ -23,7 +24,7 @@ bool Append::execute() {
 			s_IRGRP - write permission, group
 			S_IWUSR - write permission, owner
   		*/
-	return lhs->execute();
+	return lhs->execute(in, out);
 }
 
 #endif
