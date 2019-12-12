42127393a521689a7fbd21723c8246bc42c98a26
Ethan Nguyen <engeo2558@gmail.com>
2019-12-11 19:45:58 -0800

    Edited integration testing and added directory for testing

--- integration_tests/append_write_tests.sh
+++ integration_tests/append_write_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 ### append_tests_write ###
 
-echo Hello World > testTXTdir/scriptAppend.txt
-ls >> testTXTdir/scriptAppend.txt
+echo Hello World > integration_tests/scriptTest/scriptAppend.txt
+ls >> integration_tests/scriptTest/scriptAppend.txt
 ls -a >> testTXTdir/scriptAppend.txt
-echo World Hello > testTXTdir/scriptWrite.txt
-echo Word Count | wc > testTXTdir/scriptWrite.txt
+echo World Hello > integration_tests/scriptTest/scriptWrite.txt
+echo Word Count | wc > integration_tests/scriptTest/scriptWrite.txt
 exit
