3a9c13683d64a0c5f360041c38aaa7dfb0710218
Ethan Nguyen <engeo2558@gmail.com>
2019-12-11 19:50:33 -0800

    Further syntax update

--- integration_tests/append_write_tests.sh
+++ integration_tests/append_write_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 ### append_tests_write ###
 
-echo Hello World > integration_tests/scriptTest/scriptAppend.txt
-ls >> integration_tests/scriptTest/scriptAppend.txt
-ls -a >> testTXTdir/scriptAppend.txt
-echo World Hello > integration_tests/scriptTest/scriptWrite.txt
-echo Word Count | wc > integration_tests/scriptTest/scriptWrite.txt
+echo Hello World > scriptTest/scriptAppend.txt
+ls >> scriptTest/scriptAppend.txt
+ls -a >> scriptTest/scriptAppend.txt
+echo World Hello > scriptTest/scriptWrite.txt
+echo Word Count | wc > scriptTest/scriptWrite.txt
 exit
