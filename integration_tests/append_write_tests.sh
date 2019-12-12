d70efd41f484bcb0fa1049be7eb35f999cfdc002
Ethan Nguyen <engeo2558@gmail.com>
2019-12-11 19:55:51 -0800

    Fixed syntax

--- integration_tests/append_write_tests.sh
+++ integration_tests/append_write_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ### append_tests_write ###
 
-echo Hello World > scriptTest/scriptAppend.txt
+echo Hello World >> scriptTest/scriptAppend.txt
 ls >> scriptTest/scriptAppend.txt
 ls -a >> scriptTest/scriptAppend.txt
 echo World Hello > scriptTest/scriptWrite.txt
