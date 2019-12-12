fd71e26756bf01c4b871f73c07299d7f75cb0c6e
Timothy Ko <tko003@ucr.edu>
2019-12-11 21:04:33 -0800

    Fixed Pipe to have processes run in correct order.

--- src/Pipe.cpp
+++ src/Pipe.cpp
@@ -1,35 +1,30 @@
-#ifndef __PIPE_CPP__
-#define __PIPE_CPP__
-
 #include "Pipe.h"
-#include "unistd.h"
 
 bool Pipe::execute() {
     int pipefd[2];
     pid_t cpid;
     
     pipe(pipefd);
-
+    
     cpid = fork();
-    if (cpid == 0) {    // Child for left cmd
-        close(pipefd[0]);   // Close unused read end
-        dup2(pipefd[1], STDOUT_FILENO);     // Makes output go to pipe
+    if (cpid == 0) {
         close(pipefd[0]);
-        close(pipefd[1]);
+        dup2(pipefd[1], STDOUT_FILENO);
         this->lhs->execute();
         exit(1);
     }
-    else {
-        if (waitpid(cpid, NULL, 0) == -1) {
-            perror("wait error");
-        }
-        close(pipefd[1]);   // Close unused write end
-        dup2(pipefd[0], STDIN_FILENO);  // Get input from pipe
-        close(pipefd[0]);
+    cpid = fork();
+    if (cpid == 0) {
         close(pipefd[1]);
-        bool ret = this->rhs->execute();
-        return ret;
+        dup2(pipefd[0], STDIN_FILENO);
+        this->rhs->execute();
+        exit(1);
     }
+    close(pipefd[0]);
+    close(pipefd[1]);
+    
+    waitpid(-1, NULL, 0);
+    waitpid(-1, NULL, 0);
+    
+    return true;
 }
-
-#endif
