f40e662c6dd2250944979b9dcf1113c10a84cd59
Timothy Ko <tko003@ucr.edu>
2019-12-11 23:13:02 -0800

    Removed unneccessary integration test.

--- integration_tests/append_write_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-### append_tests_write ###
-
-echo Hello World >> scriptTest/scriptAppend.txt
-ls >> scriptTest/scriptAppend.txt
-ls -a >> scriptTest/scriptAppend.txt
-echo World Hello > scriptTest/scriptWrite.txt
-echo Word Count | wc > scriptTest/scriptWrite.txt
-exit
