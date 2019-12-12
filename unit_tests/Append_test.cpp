bbad45b4c8c292c81bddeca80a7a2bab8ad7aaab
Ethan Nguyen <engeo2558@gmail.com>
2019-12-11 19:38:51 -0800

    Moved folder from the root directory to unit_tests directory to keep root clean

--- unit_tests/Append_test.cpp
+++ unit_tests/Append_test.cpp
@@ -8,43 +8,43 @@
 #include "../src/Write.cpp"
 
 TEST(PipeTest, Test1) {
-	Base* com1 = new Command("cat testTXTdir/test.txt");
+	Base* com1 = new Command("cat unit_tests/testTXTdir/test.txt");
 	Base* com2 = new Command("tr a-z A-Z");
 
 	Base* pipe1 = new Pipe(com1, com2);
 	
-	Base* com3 = new Command("tee testTXTdir/newOutputFile");
+	Base* com3 = new Command("tee unit_tests/testTXTdir/newOutputFile");
 	
 	Base* pipe2 = new Pipe(pipe1, com3);
 	pipe2->execute();
 }
 
 TEST(PipeTest, Test2) {
-	Base* com1 = new Command("cat testTXTdir/test2.txt");
+	Base* com1 = new Command("cat unit_tests/testTXTdir/test2.txt");
 	Base* com2 = new Command("tr A-Z a-z");
 	
 	Base* pipe1 = new Pipe(com1, com2);
 	
-	Base* com3 = new Command("tee testTXTdir/newOutputFile2");
+	Base* com3 = new Command("tee unit_tests/testTXTdir/newOutputFile2");
 	
 	Base* pipe2 = new Pipe(pipe1, com3);
 	pipe2->execute();
 }
 
 TEST(PipeTest, Test3) {
-	Base* com1 = new Command("cat -n testTXTdir/multipleLines.txt");
+	Base* com1 = new Command("cat -n unit_tests/testTXTdir/multipleLines.txt");
 	Base* com2 = new Command("tr a-z A-Z");
 	
 	Base* pipe1 = new Pipe(com1, com2);
 	
-	Base* com3 = new Command("tee testTXTdir/newOutputFile3");
+	Base* com3 = new Command("tee unit_tests/testTXTdir/newOutputFile3");
 	
 	Base* pipe2 = new Pipe(pipe1, com3);
 	pipe2->execute();
 }
 
 TEST(AppendTest, AppTest1) {
-	Base* com1 = new Command("cat testTXTdir/input.txt");
+	Base* com1 = new Command("cat unit_tests/testTXTdir/input.txt");
 	Base* com2 = new Command("tr a-z A-Z");
 	
 	Base* pipe1 = new Pipe(com1, com2);
@@ -53,11 +53,11 @@ TEST(AppendTest, AppTest1) {
 }
 
 TEST(WriteTest, WriteTest1) {
-	Base* com1 = new Command("cat testTXTdir/input2.txt");
+	Base* com1 = new Command("cat unit_tests/testTXTdir/input2.txt");
 	Base* com2 = new Command("tr A-Z a-z");
 	
 	Base* pipe1 = new Pipe(com1, com2);
-	Base* write = new Write(pipe1, "testTXTdir/write.txt");
+	Base* write = new Write(pipe1, "unit_tests/testTXTdir/write.txt");
 	write->execute();
 }
 
